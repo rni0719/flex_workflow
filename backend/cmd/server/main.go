rni0719/test_workflow

chore: Add Dockerfiles and Docker Compose configuration for frontend and backend services

backend/cmd/server/main.go
@@ -1,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/rni0719/test_workflow/internal/app"
+	"github.com/rni0719/test_workflow/pkg/config"
+)
+
+func main() {
+	// 設定の読み込み
+	cfg := config.LoadConfig()
+	
+	// アプリケーションの初期化
+	app := app.NewApp(cfg.DatabaseURL)
+	
+	// サーバーの起動
+	app.Run(":" + cfg.Port)
+}

backend/internal/controller/workflow.go
@@ -1,0 +1,130 @@
+package controller
+
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+	"time"
+	
+	"github.com/gorilla/mux"
+	"github.com/rni0719/test_workflow/internal/model"
+	"github.com/rni0719/test_workflow/internal/repository"
+	"github.com/rni0719/test_workflow/pkg/utils"
+)
+
+type WorkflowController struct {
+	repo *repository.WorkflowRepository
+}
+
+func NewWorkflowController(repo *repository.WorkflowRepository) *WorkflowController {
+	return &WorkflowController{repo: repo}
+}
+
+func (c *WorkflowController) GetWorkflows(w http.ResponseWriter, r *http.Request) {
+	workflows, err := c.repo.GetAll()
+	if err != nil {
+		utils.RespondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	
+	utils.RespondWithJSON(w, http.StatusOK, workflows)
+}
+
+func (c *WorkflowController) CreateWorkflow(w http.ResponseWriter, r *http.Request) {
+	var workflow model.Workflow
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&workflow); err != nil {
+		utils.RespondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+	defer r.Body.Close()
+
+	workflow.CreatedAt = time.Now()
+	workflow.UpdatedAt = time.Now()
+
+	createdWorkflow, err := c.repo.Create(workflow)
+	if err != nil {
+		utils.RespondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	utils.RespondWithJSON(w, http.StatusCreated, createdWorkflow)
+}
+
+func (c *WorkflowController) GetWorkflow(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		utils.RespondWithError(w, http.StatusBadRequest, "Invalid workflow ID")
+		return
+	}
+
+	workflow, err := c.repo.GetByID(id)
+	if err != nil {
+		utils.RespondWithError(w, http.StatusNotFound, "Workflow not found")
+		return
+	}
+
+	utils.RespondWithJSON(w, http.StatusOK, workflow)
+}
+
+func (c *WorkflowController) UpdateWorkflow(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		utils.RespondWithError(w, http.StatusBadRequest, "Invalid workflow ID")
+		return
+	}
+
+	var workflow model.Workflow
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&workflow); err != nil {
+		utils.RespondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+	defer r.Body.Close()
+
+	workflow.ID = id
+	workflow.UpdatedAt = time.Now()
+
+	updatedWorkflow, err := c.repo.Update(workflow)
+	if err != nil {
+		utils.RespondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	utils.RespondWithJSON(w, http.StatusOK, updatedWorkflow)
+}
+
+func (c *WorkflowController) DeleteWorkflow(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		utils.RespondWithError(w, http.StatusBadRequest, "Invalid workflow ID")
+		return
+	}
+
+	if err := c.repo.Delete(id); err != nil {
+		utils.RespondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	utils.RespondWithJSON(w, http.StatusOK, map[string]string{"result": "success"})
+}
+
+func (c *WorkflowController) GetWorkflowTasks(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		utils.RespondWithError(w, http.StatusBadRequest, "Invalid workflow ID")
+		return
+	}
+
+	tasks, err := c.repo.GetTasks(id)
+	if err != nil {
+		utils.RespondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	utils.RespondWithJSON(w, http.StatusOK, tasks)
+}

backend/internal/models/task.go
@@ -1,0 +1,13 @@
+package model
+
+import "time"
+
+type Task struct {
+	ID          int       `json:"id"`
+	WorkflowID  int       `json:"workflow_id"`
+	Name        string    `json:"name"`
+	Description string    `json:"description"`
+	Status      string    `json:"status"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}

backend/internal/models/workflow.go
@@ -1,0 +1,12 @@
+package model
+
+import "time"
+
+type Workflow struct {
+	ID          int       `json:"id"`
+	Name        string    `json:"name"`
+	Description string    `json:"description"`
+	Status      string    `json:"status"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}

rni0719/flex_workflow

backend/cmd/server/main.go
@@ -1,0 +1,12 @@
+package main
+
+import (
+	"github.com/rni0719/flex_workflow/internal/app"
+	"github.com/rni0719/flex_workflow/pkg/config"
+)
+
+func main() {
+	cfg := config.LoadConfig()
+	a := app.NewApp(cfg.DatabaseURL)
+	a.Run(":" + cfg.Port)
+}

backend/internal/controller/workflow.go
@@ -1,0 +1,130 @@
+package controller
+
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/gorilla/mux"
+	"github.com/rni0719/flex_workflow/internal/models" // 修正点: model -> models
+	"github.com/rni0719/flex_workflow/internal/repository"
+	"github.com/rni0719/flex_workflow/pkg/utils"
+)
+
+type WorkflowController struct {
+	repo *repository.WorkflowRepository
+}
+
+func NewWorkflowController(repo *repository.WorkflowRepository) *WorkflowController {
+	return &WorkflowController{repo: repo}
+}
+
+func (c *WorkflowController) GetWorkflows(w http.ResponseWriter, r *http.Request) {
+	workflows, err := c.repo.GetAll()
+	if err != nil {
+		utils.RespondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	utils.RespondWithJSON(w, http.StatusOK, workflows)
+}
+
+func (c *WorkflowController) CreateWorkflow(w http.ResponseWriter, r *http.Request) {
+	var workflow models.Workflow // 修正点: model.Workflow -> models.Workflow
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&workflow); err != nil {
+		utils.RespondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+	defer r.Body.Close()
+
+	workflow.CreatedAt = time.Now()
+	workflow.UpdatedAt = time.Now()
+
+	createdWorkflow, err := c.repo.Create(workflow)
+	if err != nil {
+		utils.RespondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	utils.RespondWithJSON(w, http.StatusCreated, createdWorkflow)
+}
+
+func (c *WorkflowController) GetWorkflow(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		utils.RespondWithError(w, http.StatusBadRequest, "Invalid workflow ID")
+		return
+	}
+
+	workflow, err := c.repo.GetByID(id)
+	if err != nil {
+		utils.RespondWithError(w, http.StatusNotFound, "Workflow not found")
+		return
+	}
+
+	utils.RespondWithJSON(w, http.StatusOK, workflow)
+}
+
+func (c *WorkflowController) UpdateWorkflow(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		utils.RespondWithError(w, http.StatusBadRequest, "Invalid workflow ID")
+		return
+	}
+
+	var workflow models.Workflow // 修正点: model.Workflow -> models.Workflow
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&workflow); err != nil {
+		utils.RespondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+	defer r.Body.Close()
+
+	workflow.ID = id
+	workflow.UpdatedAt = time.Now()
+
+	updatedWorkflow, err := c.repo.Update(workflow)
+	if err != nil {
+		utils.RespondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	utils.RespondWithJSON(w, http.StatusOK, updatedWorkflow)
+}
+
+func (c *WorkflowController) DeleteWorkflow(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		utils.RespondWithError(w, http.StatusBadRequest, "Invalid workflow ID")
+		return
+	}
+
+	if err := c.repo.Delete(id); err != nil {
+		utils.RespondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	utils.RespondWithJSON(w, http.StatusOK, map[string]string{"result": "success"})
+}
+
+func (c *WorkflowController) GetWorkflowTasks(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		utils.RespondWithError(w, http.StatusBadRequest, "Invalid workflow ID")
+		return
+	}
+
+	tasks, err := c.repo.GetTasks(id)
+	if err != nil {
+		utils.RespondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	utils.RespondWithJSON(w, http.StatusOK, tasks)
+}

backend/internal/models/task.go
@@ -1,0 +1,13 @@
+package models
+
+import "time"
+
+type Task struct {
+	ID          int       `json:"id"`
+	WorkflowID  int       `json:"workflow_id"`
+	Name        string    `json:"name"`
+	Description string    `json:"description"`
+	Status      string    `json:"status"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}

backend/internal/models/workflow.go
@@ -1,0 +1,12 @@
+package models
+
+import "time"
+
+type Workflow struct {
+	ID          int       `json:"id"`
+	Name        string    `json:"name"`
+	Description string    `json:"description"`
+	Status      string    `json:"status"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}